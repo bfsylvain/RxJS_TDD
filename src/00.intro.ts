4706a47f0b3f8629ca76d09b0f7da059e0b667ab
bfsylvain <sylvainbonnaure@hotmail.com>
2024-04-27 17:34:38 +0200

    exercise 00 done

--- src/00.intro.ts
+++ src/00.intro.ts
@@ -1,4 +1,4 @@
-import {Observable, of} from 'rxjs';
+import {Observable, of, range} from 'rxjs';
 import {filter, map} from 'rxjs/operators';
 
 /*
@@ -6,17 +6,19 @@ import {filter, map} from 'rxjs/operators';
  */
 
 // Just an observable which should send "Hello World"
-export const helloWorldObservable = of('Hello');
+export const helloWorldObservable: Observable<string> = of('Hello World');
 
 // This one must trigger numbers from 0 to 10
 // Pro tip: You can also use `range` from RxJS
-export const zeroToTenObservable = of([0]);
+export const zeroToTenObservable: Observable<number> = range(0, 11);
 
 // Use map operator to pick the first letter au each word
-export const firstLetterObservable = of('Wild', 'Code', 'School').pipe();
+export const firstLetterObservable: Observable<string> = of('Wild', 'Code', 'School')
+.pipe(map(word => word.charAt(0)))
+;
 
 // Use filter operator to allow only positive numbers
-export const positiveNumbersObservable = of(-23, 543, 7, 6, 3, -234, 43).pipe()
+export const positiveNumbersObservable: Observable<number> = of(-23, 543, 7, 6, 3, -234, 43).pipe(filter(number => number >=0))
 
 // Write a function that produce the first 15th fibonnaci numbers
 // In mathematics, the Fibonacci numbers, commonly denoted Fn form a sequence,
@@ -32,4 +34,18 @@ export const positiveNumbersObservable = of(-23, 543, 7, 6, 3, -234, 43).pipe()
 // NOTE: Do not use `scan` operator
 export const fibonacciObservable = new Observable(function (observer) {
 
+    let prevNumber: number = 0
+    let actualNumber: number = 1
+    let newNumber: number = 1
+    const numberOfRepeats: number = 15
+
+    for(let i = 0; i < numberOfRepeats; i++){
+        observer.next(newNumber)
+        prevNumber = actualNumber
+        actualNumber = newNumber
+        newNumber = actualNumber + prevNumber
+        if(i >= numberOfRepeats-1) {
+            observer.complete()
+        }
+    }
 });
